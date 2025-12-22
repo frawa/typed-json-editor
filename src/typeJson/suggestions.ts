72036ad90d9d5cbca0bbf00ae3a02f3e9bd94aa1
Frank Wagner <agilecoderfrank@gmail.com>
2025-12-22 22:52:31 +0100

    collect keyword locations for duplicate suggested values

--- src/typeJson/suggestions.ts
+++ src/typeJson/suggestions.ts
@@ -13,11 +13,13 @@ export function suggestionsToCompletionItems(
   pos: SuggestPos,
   range: IRange,
 ): languages.CompletionItem[] {
-  const go = (location: string) => (v: any) => {
-    const pretty = JSON.stringify(v, null, 2);
-    const compact = JSON.stringify(v, null, 0);
+  type ValueLocation = { value: any; location: string };
+  const go = ({ value, location }: ValueLocation) => {
+    const pretty = JSON.stringify(value, null, 2);
+    const compact = JSON.stringify(value, null, 0);
     const label =
       compact.length > 42 ? compact.substring(0, 39) + "..." : compact;
+    const keyword = location.substring(location.lastIndexOf("/") + 1);
     const item: languages.CompletionItem = {
       kind: languages.CompletionItemKind.Value,
       label: {
@@ -26,16 +28,43 @@ export function suggestionsToCompletionItems(
         //description: TODO keyword,
         // detail: "TODO label detail",
       },
-      detail: `${location}`, //"TODO schema keyword detail",
+      detail: `${keyword}`,
       documentation: {
-        value: `\n\`\`\`${pretty}\`\`\`\n`,
+        value: `${location}\n\n\`\`\`${pretty}\`\`\`\n`,
       },
       insertText: pretty,
       range,
     };
     return item;
   };
-  return suggestions.flatMap((output) => {
-    return output.values.map(go(output.location));
+  const withLocation = suggestions.flatMap((output) =>
+    output.values.map((value) => ({ value, location: output.location })),
+  );
+  const grouped = groupBy(withLocation, ({ value }) => value);
+  const merge = (acc: ValueLocation, v: ValueLocation) => ({
+    value: v.value,
+    location: `${acc.location}\n- ${v.location}`,
   });
+  const merged = grouped
+    .values()
+    .map((vs) => vs.reduce(merge, { value: {}, location: "" }));
+  const items = merged.map(go);
+  return [...items];
+}
+
+function groupBy<K, V>(
+  vs: readonly V[],
+  by: (v: V) => K,
+): ReadonlyMap<K, readonly V[]> {
+  const result = new Map<K, V[]>();
+  vs.forEach((v) => {
+    const k = by(v);
+    const group = result.get(k);
+    if (group) {
+      result.set(k, [...group, v]);
+    } else {
+      result.set(k, [v]);
+    }
+  });
+  return result;
 }
