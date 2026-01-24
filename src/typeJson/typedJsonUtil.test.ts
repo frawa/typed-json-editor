553cddb0a539bd1740441faf9eb96971133ccae6
Frank Wagner <agilecoderfrank@gmail.com>
2026-01-24 22:27:11 +0100

    fix tests

--- src/typeJson/typedJsonUtil.test.ts
+++ src/typeJson/typedJsonUtil.test.ts
@@ -4,7 +4,7 @@ import { describe, expect, test } from "vitest";
 //     JSONDocument,
 //     TextDocument,
 // } from "vscode-json-languageservice";
-import { Node } from 'jsonc-parser';
+import { Node, parseTree } from 'jsonc-parser';
 import {
   getPointerOffsets,
   getSuggestPosAt,
@@ -13,31 +13,29 @@ import {
 } from "./typedJsonUtil";
 
 describe("typedJson utils", () => {
-  // const jsonLanguageService = getLanguageService({});
 
   function parse(text: string): Node {
-    // const td = TextDocument.create("", "json", 13, text);
-    // const doc = jsonLanguageService.parseJSONDocument(td);
-    return parseJson(text) ?? { type: 'null', offset: 0, length: 0 };
+    const result = parseTree(text);
+    return result ?? { type: 'null', offset: 0, length: 0 };
   }
 
   test("suggest pos for number", () => {
     const value = "13";
-    const doc = value;// parse(value);
+    const tree = parse(value);
     const expected = {
       pointer: "",
       inside: false,
       replaceOffset: 0,
       replaceLength: 2,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected);
-    expect(getSuggestPosAt(2, doc)).toEqual(undefined);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected);
+    expect(getSuggestPosAt(2, tree)).toEqual(undefined);
   });
 
   test("suggest pos for array", () => {
     const value = "[13,14]";
-    const doc = value; //parse(value);
+    const tree = parse(value);
     const expected0 = {
       pointer: "",
       inside: false,
@@ -68,19 +66,19 @@ describe("typedJson utils", () => {
       replaceOffset: 6,
       replaceLength: 0,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected0);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(2, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(3, doc)).toEqual(expected0b); // TODO inside true
-    expect(getSuggestPosAt(4, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(5, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(6, doc)).toEqual(expected3);
-    expect(getSuggestPosAt(7, doc)).toEqual(undefined);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected0);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(2, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(3, tree)).toEqual(expected0b); // TODO inside true
+    expect(getSuggestPosAt(4, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(5, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(6, tree)).toEqual(expected3);
+    expect(getSuggestPosAt(7, tree)).toEqual(undefined);
   });
 
   test("suggest pos for broken array", () => {
     const value = "[13";
-    const doc = value; //parse(value);
+    const tree = parse(value);
     const expected0 = {
       pointer: "",
       inside: false,
@@ -93,15 +91,15 @@ describe("typedJson utils", () => {
       replaceOffset: 1,
       replaceLength: 2,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected0);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(2, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(3, doc)).toEqual(undefined);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected0);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(2, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(3, tree)).toEqual(undefined);
   });
 
   test("suggest pos for broken array with more elements", () => {
     const value = "[13,,14";
-    const doc = value; //parse(value);
+    const tree = parse(value);
     const expected0 = {
       pointer: "",
       inside: false,
@@ -132,19 +130,19 @@ describe("typedJson utils", () => {
       replaceOffset: 5,
       replaceLength: 2,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected0);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(2, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(3, doc)).toEqual(expected0b);
-    expect(getSuggestPosAt(4, doc)).toEqual(expected0c);
-    expect(getSuggestPosAt(5, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(6, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(7, doc)).toEqual(undefined);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected0);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(2, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(3, tree)).toEqual(expected0b);
+    expect(getSuggestPosAt(4, tree)).toEqual(expected0c);
+    expect(getSuggestPosAt(5, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(6, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(7, tree)).toEqual(undefined);
   });
 
   test("suggest pos for object", () => {
     const value = '{"foo":13}';
-    const doc = value; //parse(value);
+    const tree = parse(value);
     const expected0 = {
       pointer: "",
       inside: false,
@@ -169,22 +167,22 @@ describe("typedJson utils", () => {
       replaceOffset: 9,
       replaceLength: 0,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected0);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(2, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(3, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(4, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(5, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(6, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(7, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(8, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(9, doc)).toEqual(expected3);
-    expect(getSuggestPosAt(10, doc)).toEqual(undefined);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected0);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(2, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(3, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(4, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(5, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(6, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(7, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(8, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(9, tree)).toEqual(expected3);
+    expect(getSuggestPosAt(10, tree)).toEqual(undefined);
   });
 
   test("suggest pos for broken object property value", () => {
     const value = '{"foo":}';
-    const doc = value; //parse(value);
+    const tree = parse(value);
     const expected0 = {
       pointer: "",
       inside: false,
@@ -203,19 +201,19 @@ describe("typedJson utils", () => {
       replaceOffset: 7,
       replaceLength: 0,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected0);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(2, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(3, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(4, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(5, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(6, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(7, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(8, doc)).toEqual(undefined);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected0);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(2, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(3, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(4, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(5, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(6, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(7, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(8, tree)).toEqual(undefined);
   });
   test("suggest pos for broken object only key", () => {
     const value = '{"foo" }';
-    const doc = value; //parse(value);
+    const tree = parse(value);
     const expected0 = {
       pointer: "",
       inside: false,
@@ -234,20 +232,20 @@ describe("typedJson utils", () => {
       replaceOffset: 1,
       replaceLength: 5,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected0);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(2, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(3, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(4, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(5, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(6, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(7, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(8, doc)).toEqual(undefined);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected0);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(2, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(3, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(4, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(5, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(6, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(7, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(8, tree)).toEqual(undefined);
   });
 
   test("suggest pos for samples", () => {
     const value = "{ }";
-    const doc = value; //parse(value);
+    const tree = parse(value);
     const expected0 = {
       pointer: "",
       inside: false,
@@ -266,66 +264,54 @@ describe("typedJson utils", () => {
       replaceOffset: 2,
       replaceLength: 0,
     };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected0);
-    expect(getSuggestPosAt(1, doc)).toEqual(expected1);
-    expect(getSuggestPosAt(2, doc)).toEqual(expected2);
-    expect(getSuggestPosAt(3, doc)).toEqual(undefined);
-  });
-
-  test("suggest pos for empty", () => {
-    const value = "";
-    const doc = value; //parse(value);
-    const expected = {
-      pointer: "",
-      inside: false,
-      replaceOffset: 0,
-      replaceLength: 0,
-    };
-    expect(getSuggestPosAt(0, doc)).toEqual(expected);
+    expect(getSuggestPosAt(0, tree)).toEqual(expected0);
+    expect(getSuggestPosAt(1, tree)).toEqual(expected1);
+    expect(getSuggestPosAt(2, tree)).toEqual(expected2);
+    expect(getSuggestPosAt(3, tree)).toEqual(undefined);
   });
 
   test("offsets for broken pointer", () => {
     const value = "13";
-    const doc = parse(value);
-    expect(getPointerOffsets("broken", doc)).toEqual(undefined);
+    const tree = parse(value);
+    expect(getPointerOffsets("broken", tree)).toEqual(undefined);
   });
 
   test("offsets for pointer", () => {
     const value = "13";
-    const doc = parse(value);
-    expect(getPointerOffsets("", doc)).toEqual({ offset: 0, length: 2 });
-    expect(getPointerOffsets("/", doc)).toEqual({ offset: 0, length: 2 });
+    const tree = parse(value);
+    expect(getPointerOffsets("", tree)).toEqual({ offset: 0, length: 2 });
+    expect(getPointerOffsets("/", tree)).toEqual({ offset: 0, length: 2 });
   });
   test("offsets for array", () => {
     const value = "[13,14]";
-    const doc = parse(value);
-    expect(getPointerOffsets("", doc)).toEqual({ offset: 0, length: 7 });
-    expect(getPointerOffsets("/", doc)).toEqual({ offset: 0, length: 7 });
-    expect(getPointerOffsets("/0", doc)).toEqual({ offset: 1, length: 2 });
-    expect(getPointerOffsets("/1", doc)).toEqual({ offset: 4, length: 2 });
-    expect(getPointerOffsets("/13", doc)).toEqual(undefined);
+    const tree = parse(value);
+    expect(getPointerOffsets("", tree)).toEqual({ offset: 0, length: 7 });
+    expect(getPointerOffsets("/", tree)).toEqual({ offset: 0, length: 7 });
+    expect(getPointerOffsets("/0", tree)).toEqual({ offset: 1, length: 2 });
+    expect(getPointerOffsets("/1", tree)).toEqual({ offset: 4, length: 2 });
+    expect(getPointerOffsets("/13", tree)).toEqual(undefined);
   });
   test("offsets for object", () => {
     const value = '{"foo":13}';
-    const doc = parse(value);
-    expect(getPointerOffsets("", doc)).toEqual({ offset: 0, length: 10 });
-    expect(getPointerOffsets("/", doc)).toEqual({ offset: 0, length: 10 });
-    expect(getPointerOffsets("/foo", doc)).toEqual({ offset: 7, length: 2 });
-    expect(getPointerOffsets("/1", doc)).toEqual(undefined);
-    expect(getPointerOffsets("/bar", doc)).toEqual(undefined);
+    const tree = parse(value);
+    expect(getPointerOffsets("", tree)).toEqual({ offset: 0, length: 10 });
+    expect(getPointerOffsets("/", tree)).toEqual({ offset: 0, length: 10 });
+    expect(getPointerOffsets("/foo", tree)).toEqual({ offset: 7, length: 2 });
+    expect(getPointerOffsets("/1", tree)).toEqual(undefined);
+    expect(getPointerOffsets("/bar", tree)).toEqual(undefined);
   });
   test("offsets for broken object", () => {
     const value = '{"foo":}';
-    const doc = parse(value);
-    expect(getPointerOffsets("", doc)).toEqual({ offset: 0, length: 8 });
-    expect(getPointerOffsets("/", doc)).toEqual({ offset: 0, length: 8 });
-    expect(getPointerOffsets("/foo", doc)).toEqual({ offset: 1, length: 5 });
-    expect(getPointerOffsets("/bar", doc)).toEqual(undefined);
+    const tree = parse(value);
+    expect(getPointerOffsets("", tree)).toEqual({ offset: 0, length: 8 });
+    expect(getPointerOffsets("/", tree)).toEqual({ offset: 0, length: 8 });
+    expect(getPointerOffsets("/foo", tree)).toEqual({ offset: 1, length: 5 });
+    expect(getPointerOffsets("/bar", tree)).toEqual(undefined);
   });
 
   // test("toInstance for empty sub object", () => {
   //   const value = '{"foo":{}}'
-  //   const doc = parse(value);
+  //   const tree = parse(value);
   //   expect(toInstance(doc.root!)).toEqual({ foo: {} });
   // });
 });
