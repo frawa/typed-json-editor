890c264020d6fc80c0dbd1fe0f9f73088c783069
Frank Wagner <agilecoderfrank@gmail.com>
2026-02-10 21:06:57 +0100

    use meta info from suggestions

--- src/typeJson/suggestions.ts
+++ src/typeJson/suggestions.ts
@@ -1,11 +1,33 @@
 import { IRange, languages } from "monaco-editor/esm/vs/editor/editor.api.js";
 import { SuggestPos } from "./typedJsonUtil";
 
-export type SuggestionOutput = { location: string; values: readonly any[] };
+export type SuggestionOutput =
+  { location: string; values: readonly any[] }
+  | SuggestionGroup;
+
+export type SuggestionGroup =
+  { meta: readonly string[]; group: readonly SuggestionOutput[] };
+
+function isGroup(o: SuggestionOutput): o is SuggestionGroup {
+  return (o as SuggestionGroup).meta !== undefined;
+}
 
 export function parseSuggestionOutput(json: any): readonly SuggestionOutput[] {
   // TODO decoding
   return json as SuggestionOutput[];
+}
+
+export type ValueWithMeta = { value: any; locations: readonly string[]; meta: readonly string[] };
+
+function addMeta(meta: readonly string[], o: SuggestionOutput): readonly ValueWithMeta[] {
+  if (isGroup(o)) {
+    const { meta, group } = o;
+    return group.flatMap(v => addMeta(meta, v));
+  } else {
+    const { location, values } = o;
+    const locations = [location];
+    return values.map(value => ({ value, locations, meta }));
+  }
 }
 
 export function suggestionsToCompletionItems(
@@ -13,13 +35,16 @@ export function suggestionsToCompletionItems(
   pos: SuggestPos,
   range: IRange,
 ): languages.CompletionItem[] {
-  type ValueLocation = { value: any; location: string };
-  const go = ({ value, location }: ValueLocation) => {
+  const go = ({ value, locations, meta }: ValueWithMeta) => {
     const pretty = JSON.stringify(value, null, 2);
     const compact = JSON.stringify(value, null, 0);
     const label =
       compact.length > 42 ? compact.substring(0, 39) + "..." : compact;
+    const location = locations?.[0] ?? '';
     const keyword = location.substring(location.lastIndexOf("/") + 1);
+    const docMeta = meta.join("\n");
+    const docLocations = locations.join("\n");
+    const doc = [docMeta, docLocations].join("\n\n")
     const item: languages.CompletionItem = {
       kind: languages.CompletionItemKind.Value,
       label: {
@@ -30,26 +55,30 @@ export function suggestionsToCompletionItems(
       },
       detail: `${keyword}`,
       documentation: {
-        value: `${location}\n\n\`\`\`${pretty}\`\`\`\n`,
+        value: `${doc}\n\n\`\`\`${pretty}\`\`\`\n`,
       },
       insertText: pretty,
       range,
     };
     return item;
   };
-  const withLocation = suggestions.flatMap((output) =>
-    output.values.map((value) => ({ value, location: output.location })),
-  );
-  const grouped = groupBy(withLocation, ({ value }) => value);
-  const merge = (acc: ValueLocation, v: ValueLocation) => ({
+  const items = toValueWithMeta(suggestions).map(go);
+  return [...items];
+}
+
+export function toValueWithMeta(suggestions: readonly SuggestionOutput[]): readonly ValueWithMeta[] {
+  const withMeta: ValueWithMeta[] = suggestions.flatMap(o => addMeta([], o));
+  const grouped = groupBy(withMeta, ({ value }) => value);
+  const merge = (acc: ValueWithMeta, v: ValueWithMeta) => ({
     value: v.value,
-    location: `${acc.location}\n- ${v.location}`,
+    locations: [...acc.locations, ...v.locations],
+    meta: [...acc.meta, ...v.meta],
   });
   const merged = grouped
     .values()
-    .map((vs) => vs.reduce(merge, { value: {}, location: "" }));
-  const items = merged.map(go);
-  return [...items];
+    .map((vs) => vs.reduce(merge, { value: {}, locations: [], meta: [] }))
+    .toArray();
+  return merged;
 }
 
 function groupBy<K, V>(
